58976bf93b84309b52ec81170f58a5f17022e0fe
Prashant Shukla <android.prashant.ps@gmail.com>
2024-05-22 17:15:11 +0530

    Basic starting on Panel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Table;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Table::configureUsing(function (Table $table): void {
+            $table
+                ->emptyStateHeading('No data yet')
+                ->striped()
+                ->defaultPaginationPageOption(10)
+                ->paginated([10, 25, 50, 100])
+                ->extremePaginationLinks()
+                ->defaultSort('created_at', 'desc');
+        });
     }
 }
