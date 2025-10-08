eb0c97e4035889c53d83b21fe35dee2ffb87651a
deependrasingh80 <deependrasingh3043@gmail.com>
2025-10-08 17:52:34 +0530

    blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -190,9 +190,6 @@
                     <a href="{{ url('/blogs') }}"
                         class="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors font-medium">Blogs</a>
 
-                    <a href="{{ url('/blogs') }}"
-                        class="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors font-medium">Blogs</a>
-
                     <a href="{{ url('/placement') }}"
                         class="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors font-medium">Jobs</a>
 
